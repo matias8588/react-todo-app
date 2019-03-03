4789994a4fd76793bcf7a46ce8b634cc3a99d2d2
Matias <matiascejas8@gmail.com>
2019-03-03 11:29:39 -0300

    migracion a create react app

--- README.md
+++
@@ -1,8 +0,0 @@
-# ToDO App
-
-**ES**
--Aplicación para crear, editar, listar y eliminar tareas. Realizada con el stack MERN.
-
-**En**
--
-Application to create, edit, list and delete tasks. Built with the MERN stack.
--- src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const URI = 'mongodb://localhost/mern-tareas'
-
-mongoose.connect(URI)
-  .then(db => console.log('DB conectada'))
-  .catch(err => console.error(err))
-
-module.exports = mongoose
