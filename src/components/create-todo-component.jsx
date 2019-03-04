e6fc99ac8982fdb702b3128d6cc22493ef3e85d2
Matias <matiascejas8@gmail.com>
2019-03-04 05:58:01 -0300

    front end basico terminado

--- src/components/create-todo-component.jsx
+++ src/components/create-todo-component.jsx
@@ -66,6 +66,52 @@ export default class CreateTodo extends Component {
               onChange={this.onChangeTodoDescription}
             />
           </div>
+          <div className='form-group'>
+            <label>Responsable: </label>
+            <input type='text'
+              className='form-control'
+              value={this.state.todo_responsible}
+              onChange={this.onChangeTodoResponsible}
+            />
+          </div>
+          <div className='form-group'>
+            <div className='form-check form-check-inline'>
+              <input className='form-check-input'
+                type='radio'
+                name='priorityOptions'
+                id='prorityBaja'
+                value='Baja'
+                checked={this.state.todo_priority === 'Baja'}
+                onChange={this.onChangeTodoPriotity}
+              />
+              <label className='form-check-label'>Baja</label>
+            </div>
+            <div className='form-check form-check-inline'>
+              <input className='form-check-input'
+                type='radio'
+                name='priorityOptions'
+                id='prorityMedia'
+                value='Media'
+                checked={this.state.todo_priority === 'Media'}
+                onChange={this.onChangeTodoPriotity}
+              />
+              <label className='form-check-label'>Media</label>
+            </div>
+            <div className='form-check form-check-inline'>
+              <input className='form-check-input'
+                type='radio'
+                name='priorityOptions'
+                id='prorityAlta'
+                value='Alta'
+                checked={this.state.todo_priority === 'Alta'}
+                onChange={this.onChangeTodoPriotity}
+              />
+              <label className='form-check-label'>Alta</label>
+            </div>
+          </div>
+          <div className='form-group'>
+            <input type='submit' value='Create ToDo' className='btn btn-primary' />
+          </div>
         </form>
       </div>
     )
